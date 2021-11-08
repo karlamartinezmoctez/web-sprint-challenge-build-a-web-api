7499b4faa5ee4a2fb770423ed601632060e1ae18
Karla Martinez <karlamartinezmoc@gmail.com>
2021-11-07 22:24:24 -0700

    did branch in main

--- api/server.js
+++ api/server.js
@@ -1,9 +1,30 @@
-const express = require('express');
-const server = express();
-
 // Configure your server here
 // Build your actions router in /api/actions/actions-router.js
 // Build your projects router in /api/projects/projects-router.js
 // Do NOT `server.listen()` inside this file!
+
+const express = require('express');
+const server = express();
+
+const actions = require('./actions/actions-router')
+const projects = require('./projects/projects-router')
+
+server.use(express.json())
+server.use(logger)
+server.use('/api/actions', actions)
+server.use('/api/projects', projects)
+
+server.get('/', (req, res) => {
+    res.status(200).send(`<h1>Welcome to the API</h1>`)
+})
+
+
+function logger(req, res, next) {
+    const newTime = new Date()
+    const timestamp = newTime.toLocaleString()
+    console.log(`[${timestamp}]: ${req.method} called to ${req.url}`)
+    console.log('\n --- Start Body ---\n', req.body, '\n --- End Body --- \n')
+    next()
+}
 
 module.exports = server;
